bda8352dadeb008e0a2c15e62ba5b47f2911f91d
AfonsoDD <153368316+AfonsoDD@users.noreply.github.com>
2025-04-06 22:57:23 +0200

    All the steps finished; Corrected variables that were being globally stored to their correct local environments; Functional game for 5 rounds with winner messages for round and game; To add: 1- Quit functionality - exit the program; 2- Play again functionality - reset global variables; 3- Player name functionality - updated logs according to name;

--- script.js
+++ script.js
@@ -1,28 +1,81 @@
-console.log ('Hello world!')
+// console.log ('Hello world!')
 
 // Step 2 - Logic for the computer choice 
 
 function getComputerChoice () {
 
     let computerRandomizedPlay = Math.random(); // Returns a random number ]0,1]
-    let computerPlay;// Setting the variable that will hold the computer's play
+    let computerChoice = "";// Setting the variable that will hold the computer's play (locally stored)
 
     if (computerRandomizedPlay <= 1/3) { // A third of the time its "Rock"
-        computerPlay = "Rock";
+        computerChoice = "ROCK";
     }
     else if (computerRandomizedPlay > 1/3 && computerRandomizedPlay <= 2/3) { // Another third is "Paper"
-        computerPlay = "Paper";
+        computerChoice = "PAPER";
     }
     else { // The rest is "Scissors"
-        computerPlay = "Scissors";
+        computerChoice = "SCISSOR";
     }
-    return console.log(computerRandomizedPlay, computerPlay)
+    return computerChoice // Need to save this in a global variable
 }
 
 // Step 3 - Logic for the human choice
 
 function getHumanChoice () {
-    humanPlay = prompt("What is your play ? -");
-    return humanPlay;
+    let humanChoice = prompt("What is your choice ? -").toUpperCase(); // Asking the user for his play
+    return humanChoice; // Need to save this in a global variable;
 }
 
+// Step 4 - Declare players variables;
+
+let humanScore = 0;
+let computerScore = 0; // I think there is no need to define this globally; We could do it in playGame()
+
+// Step 5 - Logic for a single round;
+
+function playRound (computerSelection, humanSelection) {
+
+    let roundMessage = "";
+
+    if (computerSelection == humanSelection) { // Draw validation
+        roundMessage = "We have a draw!"
+    } 
+    
+    else if ( // Player won
+        (computerSelection == "ROCK" && humanSelection == "PAPER") || 
+        (computerSelection == "PAPER" && humanSelection == "SCISSOR") ||
+        (computerSelection == "SCISSOR" && humanSelection == "ROCK")) {
+
+        roundMessage = "You win!";
+        humanScore += 1;
+
+    } else { // Player lost
+        roundMessage = "You lose!";
+        computerScore += 1;
+    }
+
+    return console.log(roundMessage)
+}
+
+function playGame () {
+
+    let round = 0;
+
+    while (round < 5) {
+        computerSelection = getComputerChoice(); // define the play for computer
+        humanSelection = getHumanChoice(); // define the play for player
+        playRound(computerSelection, humanSelection); // play the round
+        round += 1; // update round counter
+        console.log(`Player - ${humanSelection}. Computer - ${computerSelection}.\nPlayer has ${humanScore} points. Computer has ${computerScore} points.\nThis was round ${round}/5.`)
+    }
+
+    if (humanScore > computerScore) { // log the victory screen
+        return console.log("Player wins - Human Supremacy")
+    } else if (humanScore < computerScore) {
+        return console.log("Computer wins - We are fuck*d")
+    } else {
+        return console.log("A draw... boring")
+    }
+}
+
+playGame()
