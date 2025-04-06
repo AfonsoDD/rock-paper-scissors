8b1ad0d9d88fc5a0d188d513bfd42ce253299c82
AfonsoDD <153368316+AfonsoDD@users.noreply.github.com>
2025-04-06 11:17:21 +0200

    Edited the title of the HTML File; Added the logic to get the ComputerPlay;

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log ('Hello world!')
+
+// Logic for the computer choice
+
+function getComputerChoice () {
+
+    let computerRandomizedPlay = Math.random(); // Returns a random number ]0,1]
+    let computerActualPlay;// Setting the variable that will hold the computer's play
+
+    if (computerRandomizedPlay <= 1/3) {
+        computerActualPlay = "Rock";
+    }
+    else if (computerRandomizedPlay > 1/3 && computerRandomizedPlay <= 2/3) {
+        computerActualPlay = "Paper";
+    }
+    else {
+        computerActualPlay = "Scissors";
+    }
+    return console.log(computerRandomizedPlay, computerActualPlay)
+}
