a2b978446813ec268be7d2387c19b5c47ec0a22f
AfonsoDD <153368316+AfonsoDD@users.noreply.github.com>
2025-04-13 00:28:30 +0200

    Simple UI added.

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
-// console.log ('Hello world!')
+// UI for Rock-Paper-Scissors
 
-// Step 2 - Logic for the computer choice 
+// Step 1 - Set a Play Button!;
+
+const startBtn = document.createElement("button");
+startBtn.textContent = "Start a game";
+document.body.appendChild(startBtn);
+
+
+// Step 2 - Set up the playing buttons;
+
+const btnContainer = document.querySelector("#button-container");
+
+const rock = document.createElement("button");
+rock.textContent = "ROCK";
+
+const paper = document.createElement("button");
+paper.textContent = "PAPER";
+
+const scissors = document.createElement("button");
+scissors.textContent = "SCISSORS"
+
+// Use the Start button to intialize the variables, display the buttons and change it to a reset game button;
+startBtn.addEventListener("click", () => {
+
+    // initilizing / resetting global variables;
+    humanScore = 0;
+    computerScore = 0;
+    round = 0; 
+
+    if (!document.body.contains(btnContainer)) { // adds the button container if you reset a game
+        document.body.append(btnContainer)
+     } 
+
+    btnContainer.append(rock, paper, scissors) // inserts buttons to play
+
+    startBtn.textContent = "New Game" // updates the button text
+
+    if (document.body.contains(results)) { // removes the messages from previous game, if it exists
+        document.body.removeChild(results)
+     } 
+});
+
+// Results and winner divs to display text;
+
+const results = document.createElement("div"); // the container for results
+results.className = "results-container";
+
+const winnerMessage = document.createElement("div");
+winnerMessage.className = "winner-message";
+
+// Generate Computer's play (same as before);
 
 function getComputerChoice () {
 
@@ -19,63 +68,64 @@ function getComputerChoice () {
     return computerChoice // Need to save this in a global variable
 }
 
-// Step 3 - Logic for the human choice
+// Set the functionality of the buttons for the player
 
-function getHumanChoice () {
-    let humanChoice = prompt("What is your choice ? -").toUpperCase(); // Asking the user for his play
-    return humanChoice; // Need to save this in a global variable;
+function btnHumanChoice (button) {
+    let humanSelection = button.textContent;
+    let computerSelection = getComputerChoice();
+    return playRound(computerSelection, humanSelection)
 }
 
-// Step 4 - Declare players variables;
+rock.addEventListener("click", () => btnHumanChoice(rock));
+paper.addEventListener("click", () => btnHumanChoice(paper));
+scissors.addEventListener("click", () => btnHumanChoice(scissors));
 
-let humanScore = 0;
-let computerScore = 0; // I think there is no need to define this globally; We could do it in playGame()
-
-// Step 5 - Logic for a single round;
+// The same playRound function as before;
 
 function playRound (computerSelection, humanSelection) {
 
     let roundMessage = "";
 
-    if (computerSelection == humanSelection) { // Draw validation
-        roundMessage = "We have a draw!"
-    } 
-    
-    else if ( // Player won
-        (computerSelection == "ROCK" && humanSelection == "PAPER") || 
-        (computerSelection == "PAPER" && humanSelection == "SCISSOR") ||
-        (computerSelection == "SCISSOR" && humanSelection == "ROCK")) {
-
-        roundMessage = "You win!";
-        humanScore += 1;
-
-    } else { // Player lost
-        roundMessage = "You lose!";
-        computerScore += 1;
-    }
-
-    return console.log(roundMessage)
-}
-
-function playGame () {
-
-    let round = 0;
+    document.body.appendChild(results);
 
     while (round < 5) {
-        computerSelection = getComputerChoice(); // define the play for computer
-        humanSelection = getHumanChoice(); // define the play for player
-        playRound(computerSelection, humanSelection); // play the round
-        round += 1; // update round counter
-        console.log(`Player - ${humanSelection}. Computer - ${computerSelection}.\nPlayer has ${humanScore} points. Computer has ${computerScore} points.\nThis was round ${round}/5.`)
-    }
 
-    if (humanScore > computerScore) { // log the victory screen
-        return console.log("Player wins - Human Supremacy")
-    } else if (humanScore < computerScore) {
-        return console.log("Computer wins - We are fuck*d")
-    } else {
-        return console.log("A draw... boring")
+        if (computerSelection == humanSelection) { // Draw validation
+            roundMessage = `We have a draw! You played - ${humanSelection}, Computer played - ${computerSelection}`
+            round += 1
+        } 
+        else if ( // Player won
+            (computerSelection == "ROCK" && humanSelection == "PAPER") || 
+            (computerSelection == "PAPER" && humanSelection == "SCISSOR") ||
+            (computerSelection == "SCISSOR" && humanSelection == "ROCK")) {       
+            roundMessage = `You win! You played - ${humanSelection}, Computer played - ${computerSelection}`;
+            humanScore += 1;
+            round += 1;   
+        } else { // Player lost
+            roundMessage = `You lose! You played - ${humanSelection}, Computer played - ${computerSelection}`;
+            computerScore += 1;
+            round += 1
+        }
+
+        if (round < 5) { // Display results for every round but the last one
+            return results.textContent =
+            `${roundMessage}
+            The current score is:
+            Player - ${humanScore}
+            Computer - ${computerScore}
+            This was round ${round}/5`;
+
+        } else { // Display results for the last round
+
+            btnContainer.remove(rock, paper, scissors)
+
+            if (humanScore > computerScore) { // log the victory screen
+                return results.textContent = `Player wins - Human Supremacy! Final score was ${humanScore} to ${computerScore}.`
+            } else if (humanScore < computerScore) {
+                return results.textContent = `Computer wins - We are fuck*d. Final score was ${humanScore} to ${computerScore}.`
+            } else {
+                return results.textContent = `A draw... boring. Final score was ${humanScore} to ${computerScore}.`
+            }
+        }
     }
 }
-
-playGame()
